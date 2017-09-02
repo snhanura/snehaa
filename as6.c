925c8c8236cc8a54845ebaaed010e88bf7584bf7
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 19:34:33 -0700

    Update as6.c

--- as6.c
+++ as6.c
@@ -6,10 +6,10 @@ printf("enter one year");
 scanf("%d"&n);
 if(a%4==0)
 {
-printf("this year is a leap yera");
+printf("this year is a leap year");
 }
 else
 {
-printf("this year is not a leap yaer");
+printf("this year is not a leap year");
 }
 getch();}
