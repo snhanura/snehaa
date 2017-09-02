feedcd937149cf10a0d01077a8c084671659e230
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 21:12:34 -0700

    Create as11.c

---
+++ as11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int pow,num;
+printf("enter the pow and number");
+scanf("%d,%d",&pow,&num);
+for(i<0;i<pow;i++)
+{
+mul=1*num;
+mul=mul1;
+as=mul*mul1;
+}
+printf("%d",as);
+}
+getch();
+}
