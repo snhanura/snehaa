ea1ec6b9ecc34941b615c47c8b69260ee4f89670
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 19:32:14 -0700

    Create as6.c

---
+++ as6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter one year");
+scanf("%d"&n);
+if(a%4==0)
+{
+printf("this year is a leap yera");
+}
+else
+{
+printf("this year is not a leap yaer");
+}
+getch();}
