4488434458643bd0b863ddc288e592ee3ce9666f
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 21:28:38 -0700

    Create as12.c

---
+++ as12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,n1,sum;
+printf("enter the number");
+sacnf("%d",n0;
+while(n>0)
+{
+or=n;
+n1=n%10;
+n=n1/10;
+if(or==n)
+{
+printf("it is palindrome");
+}else
+{
+printf("it is not a palindrome");
+}
+}
